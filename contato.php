dd542fe272b1f8103f3bdebfab8d78c80488c906
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-10-02 14:33:55 -0300

    Protege contra exploits de emails.

--- contato.php
+++ contato.php
@@ -13,27 +13,40 @@
 
     if((isset($_POST)) && (isset($_POST['nome'])))
     {
-        $destinatario = '';
+        $destinatario = "";
 
         if((isset($_POST['nome'])) && ($_POST['nome'] != ""))
-            $nome = $_POST['nome'];
+            $nome = stripslashes($_POST['nome']);
         else
-            $errNome = "O campo nome Ã© obrigatÃ³rio";
+            $errNome = "O campo nome é obrigatório";
 
         if((isset($_POST['email'])) && ($_POST['email'] != ""))
-            $email = $_POST['email'];
+        {
+            $email = stripslashes($_POST['email']);
+            $email = preg_replace("([\r\n])", "", $email);
+        }
         else
-            $errEmail = "O campo email Ã© obrigatÃ³rio";
+            $errEmail = "O campo email é obrigatório";
 
         if(isset($_POST['telefone']))
-            $telefone = $_POST['telefone'];
+            $telefone = stripslashes($_POST['telefone']);
 
         if((isset($_POST['mensagem'])) && ($_POST['mensagem'] != ""))
-            $mensagem = $_POST['mensagem'];
+            $mensagem = stripslashes($_POST['mensagem']);
         else
-            $errMensagem = "O campo mensagem Ã© obrigatÃ³rio";
+            $errMensagem = "O campo mensagem é obrigatório";
 
-        if((!$errNome) && (!$errEmail) && (!$errMensagem))
+        $injection = "/(to:|from:|cc:|bcc:|content-type)/i";
+
+        if ((preg_match($injection, $nome)) ||
+            (preg_match($injection, $email)) ||
+            (preg_match($injection, $telefone)) ||
+            (preg_match($injection, $mensagem)))
+            $success = 'Seu formulário continha email injections.';
+        else
+            $injection = false;
+
+        if((!$errNome) && (!$errEmail) && (!$errMensagem) && (!$injection))
         {
             $novaMensagem = $mensagem;
 
@@ -68,7 +81,7 @@
                 <div>
                     <span class="erro"><?php if($errEmail) echo $errEmail; ?></span>
                     <label for="email">Email:</label>
-                    <input name="email" type="email" <?php if($email) echo 'value="'.$email.'"' ?> placeholder="Escreva seu endereÃ§o de email" required />
+                    <input name="email" type="email" <?php if($email) echo 'value="'.$email.'"' ?> placeholder="Escreva seu endereço de email" required />
                 </div>
                 <div>
                     <label for="telefone">Telefone:</label>
@@ -77,7 +90,7 @@
                 <div>
                     <span class="erro"><?php if($errMensagem) echo $errMensagem; ?></span>
                     <label for="mensagem">Mensagem:</label>
-                    <textarea name="mensagem" placeholder="Escreva sua dÃºvida ou sugestÃ£o" required ><?php if($mensagem) echo $mensagem ?></textarea>
+                    <textarea name="mensagem" placeholder="Escreva sua dúvida ou sugestão" required ><?php if($mensagem) echo $mensagem ?></textarea>
                 </div>
                 <input type="submit" value="Enviar Email" />
             </fieldset>
